98f8451a2cfb18a743b49b08eb035619e8f71442
PovilasKorop <povilask007@gmail.com>
2021-08-02 07:43:49 +0300

    first commit

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Question::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'question' => $this->faker->text(20) . '?',
+            'user_id' => User::inRandomOrder()->first()->id,
+        ];
+    }
+}
---
+++ tests/Feature/VoicesTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Question;
+use App\Models\User;
+use App\Models\Voice;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class VoicesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_voice_post_is_not_public()
+    {
+        $response = $this->postJson('api/voices',
+            ['question_id' => 1, 'value' => 1]);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_can_post_voice_on_someone_elses_question()
+    {
+        $question_user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $question_user->id]);
+
+        $voice_user = User::factory()->create();
+        $response = $this->actingAs($voice_user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id, 'value' => 1]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status'=>200,
+            'message'=>'Voting completed successfully'
+        ]);
+        $this->assertDatabaseHas(Voice::class,
+            ['user_id' => $voice_user->id, 'question_id' => $question->id, 'value' => 1]);
+    }
+
+    public function test_cannot_post_voice_on_nonexisting_question()
+    {
+        $question_user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $question_user->id]);
+
+        $voice_user = User::factory()->create();
+        $response = $this->actingAs($voice_user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id + 1, 'value' => '1']);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_cannot_post_voice_on_your_own_question()
+    {
+        $user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id, 'value' => '1']);
+
+        $response->assertJson([
+            'message' => 'The user is not allowed to vote to your question'
+        ]);
+    }
+
+    public function test_cannot_post_same_voice_twice()
+    {
+        $question_user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $question_user->id]);
+
+        $voice_user = User::factory()->create();
+        $this->actingAs($voice_user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id, 'value' => '1']);
+
+        $response = $this->actingAs($voice_user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id, 'value' => '1', 'aaa' => 'bbb']);
+
+        $response->assertJson([
+            'status' => 500,
+            'message' => 'The user is not allowed to vote more than once'
+        ]);
+    }
+
+    public function test_can_update_your_own_voice()
+    {
+        $question_user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $question_user->id]);
+
+        $voice_user = User::factory()->create();
+        $this->actingAs($voice_user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id, 'value' => '1']);
+
+        $response = $this->actingAs($voice_user, 'api')->postJson('api/voices',
+            ['question_id' => $question->id, 'value' => '0']);
+
+        $response->assertJson([
+            'status'=>201,
+            'message'=>'update your voice'
+        ]);
+    }
+}
